82789d72a599389c5258cbfc1c1613a4be06daf5
Andy Choens <andy.choens@gmail.com>
2023-03-13 16:01:22 -0400

    Nearly everything here changed.

--- dependencies.R
+++ dependencies.R
@@ -1,9 +1,16 @@
 ## Simple script used to install all packages needed by the lecture/lab.
 
 p <- c(
+  "janitor",
   "knitr",
+  "lubridate",
   "markdown",
+  "modelr",
+  "rmarkdown",
+  "shiny",
   "tidyverse",
   "Stat2Data"
 )
 install.packages(p)
+
+unlink("lab-answers.Rmd")
